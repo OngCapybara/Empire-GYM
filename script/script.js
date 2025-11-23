cfc8b696943fdeced2cbced525d0e2df41ac67e3
DevilTanuki99 <wibuakun2596@gmail.com>
2025-11-24 00:25:56 +0700

    Initial commit Empire GYM

---
+++ script/script.js
@@ -0,0 +1,23 @@
+// js/main.js
+
+// Navbar highlight saat scroll
+const sections = document.querySelectorAll('section');
+const navLi = document.querySelectorAll('header nav ul li a');
+
+window.addEventListener('scroll', () => {
+    let current = '';
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+        if (pageYOffset >= sectionTop - sectionHeight / 3) {
+            current = section.getAttribute('id');
+        }
+    });
+
+    navLi.forEach(a => {
+        a.classList.remove('active');
+        if (a.getAttribute('href') === '#' + current) {
+            a.classList.add('active');
+        }
+    });
+});
