3b1efb7143dbf1e989cb537238e2f79b784c18b8
DevilTanuki99 <wibuakun2596@gmail.com>
2025-11-24 03:11:59 +0700

    Add maps, time and update js

--- script/script.js
+++ script/script.js
@@ -21,3 +21,37 @@ window.addEventListener('scroll', () => {
         }
     });
 });
+
+
+function checkGymStatus() {
+    const now = new Date();
+    const day = now.getDay(); // 0 = Minggu, 1 = Senin ... 6 = Sabtu
+    const hour = now.getHours();
+    const minutes = now.getMinutes();
+    const statusEl = document.getElementById('open-status');
+
+    let openHour, closeHour;
+
+    if(day >= 1 && day <= 5){ // Senin - Jumat
+        openHour = 7;  // 07:30
+        closeHour = 21; // 21:00
+    } else { // Sabtu - Minggu
+        openHour = 7;  // 07:30
+        closeHour = 19; // 19:00
+    }
+
+    const nowMinutes = hour*60 + minutes;
+    const openMinutes = openHour*60 + 30; // 07:30
+    const closeMinutes = closeHour*60; // misal 21:00
+
+    if(nowMinutes >= openMinutes && nowMinutes <= closeMinutes){
+        statusEl.textContent = "OPEN";
+        statusEl.style.color = "limegreen";
+    } else {
+        statusEl.textContent = "CLOSED";
+        statusEl.style.color = "red";
+    }
+}
+
+// Jalankan saat load
+window.onload = checkGymStatus;
